261f0de68e7e2fe71e034073d730c7de13a446e3
EmnaBenRached <b.rached.emna@gmail.com>
2023-09-22 22:00:33 -0400

    Add style.css in public , delete Home.css, modify Home.vue ,

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -33,8 +33,28 @@
                         <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
                     </a>
                 </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
+
+                <div class = "restaurant_info">
+                    <h2>Restaurant A</h2>
+                    <p>Address: 123 Republic Street</p>
+                    <h3>Price Range: moderate</h3>
+                    <h3>Genre: Italian</h3>
+                    <div class = "restaurant_star">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class = "restaurant_card">
+                <div class = "restaurant_image">
+                    <a href = "#">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
+                    </a>
                 </div>
                 <div class = "restaurant_info">
                     <h2>Restaurant A</h2>
@@ -58,9 +78,7 @@
                         <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
                     </a>
                 </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
-                </div>
+
                 <div class = "restaurant_info">
                     <h2>Restaurant A</h2>
                     <p>Address: 123 Republic Street</p>
@@ -83,34 +101,7 @@
                         <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
                     </a>
                 </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
-                </div>
-                <div class = "restaurant_info">
-                    <h2>Restaurant A</h2>
-                    <p>Address: 123 Republic Street</p>
-                    <h3>Price Range: moderate</h3>
-                    <h3>Genre: Italian</h3>
-                    <div class = "restaurant_star">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
-                    </div>
 
-                </div>
-            </div>
-
-            <div class = "restaurant_card">
-                <div class = "restaurant_image">
-                    <a href = "#">
-                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
-                    </a>
-                </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
-                </div>
                 <div class = "restaurant_info">
                     <h2>Restaurant A</h2>
                     <p>Address: 123 Republic Street</p>
@@ -140,8 +131,27 @@
                         <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
                     </a>
                 </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
+                <div class = "restaurant_info">
+                    <h2>Restaurant A</h2>
+                    <p>Address: 123 Republic Street</p>
+                    <h3>Price Range: moderate</h3>
+                    <h3>Genre: Italian</h3>
+                    <div class = "restaurant_star">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class = "restaurant_card">
+                <div class = "restaurant_image">
+                    <a href = "#">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
+                    </a>
                 </div>
                 <div class = "restaurant_info">
                     <h2>Restaurant A</h2>
@@ -165,9 +175,6 @@
                         <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
                     </a>
                 </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
-                </div>
                 <div class = "restaurant_info">
                     <h2>Restaurant A</h2>
                     <p>Address: 123 Republic Street</p>
@@ -190,34 +197,7 @@
                         <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
                     </a>
                 </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
-                </div>
-                <div class = "restaurant_info">
-                    <h2>Restaurant A</h2>
-                    <p>Address: 123 Republic Street</p>
-                    <h3>Price Range: moderate</h3>
-                    <h3>Genre: Italian</h3>
-                    <div class = "restaurant_star">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
-                    </div>
 
-                </div>
-            </div>
-
-            <div class = "restaurant_card">
-                <div class = "restaurant_image">
-                    <a href = "#">
-                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/06/9e/e9/13/carre-d-agneau.jpg"  alt="Restaurant A Image">
-                    </a>
-                </div>
-                <div class = "small_card">
-                    <i class = "fa-solid fa-heart"></i>
-                </div>
                 <div class = "restaurant_info">
                     <h2>Restaurant A</h2>
                     <p>Address: 123 Republic Street</p>
@@ -245,80 +225,3 @@
   </div>
 </template>
 
-<style>
-/* Restaurant */
-
-.restaurant {
-    width : 100; 
-    
-}
-
-.restaurant h1{
-    font-size: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 30px;
-}
-
-.restaurant h1 span {
-    color: rgb(253,57,8);
-    margin-left: 15PX;
-    font-family: mv boli;
-}
-
-.restaurant .restaurant_box {
-    width:95%;
-    margin :0 auto ;
-    display: flex;
-    padding: 10px 10px;
-    
-}
-
-.restaurant .restaurant_box .restaurant_card {
-    width:325px;
-    height:380px;
-    padding-top: 10px;
-    margin-right: 20px;
-    margin-bottom: 20px;
-    box-shadow: 0px 10px 10px rgba(0,0,0,0.2);
-    
-}
-
-.restaurant .restaurant_box .restaurant_card .restaurant_image {
-    width:300px;
-    height:200px;  
-    margin: 0 auto;
-    overflow:hidden;
-    object-position: center;
-}
-
-.restaurant .restaurant_box .restaurant_card .restaurant_image img{
-    width:100%;
-    height: 100%;
-    object-fit: cover;
-    transition: 0.3s;
-}
-.restaurant .restaurant_box .restaurant_card .restaurant_image:hover img {
-    transform: scale(1.1);
-}
-
-.restaurant .restaurant_box .restaurant_card .small_card {
-    width:45px;
-    height:45px;
-    float: right;
-    position: relative;
-    top:-240px;
-    right:-8px;
-    opacity:1;
-    border-radius: 7px;
-}
-
-
-
-</style>
-
-
-
-
-uyh
