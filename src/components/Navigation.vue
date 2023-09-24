a6565deb8be4a3ded2413827fcab8abf70bd58f8
Jerome <jelelo.laurence@hotmail.com>
2023-09-23 21:50:58 -0400

    added bootstrap navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,24 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/">Home</router-link>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <router-link class="navbar-brand" to="/#">UFood</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/restaurant">Restaurants</router-link>
       </li>
-      <li>
-        <router-link to="/restaurant">Restaurant</router-link>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/user">User Profile</router-link>
       </li>
-      <li>
-        <router-link to="/user">User Profile</router-link>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/login">Login</router-link>
       </li>
     </ul>
-  </nav>
+  </div>
+</nav>
 </template>
 
-<style></style>
+<style>
+</style>
