f884739586028f445356f72287bfaa9fbc1bf966
Jerome <jelelo.laurence@hotmail.com>
2023-09-25 13:48:53 -0400

    login/logout (hard codé)

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,30 @@
+<script>
+
+  export default{
+    name: "Navigation",
+    data() {
+      return {
+        txt_user: "",
+        txt_logout: "",
+        txt_login: "Login",
+      }
+    },
+    methods: {
+      disableLoginBtn(event){
+        this.txt_user = "Admin"
+        this.txt_login = ""
+        this.txt_logout = "Logout"
+      },
+      disableLogoutBtn(event){
+        this.txt_user = ""
+        this.txt_login = "Login"
+        this.txt_logout = ""
+      }
+    }
+    
+  }
+</script>
+
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
   <router-link class="navbar-brand" to="/">UFood</router-link>
@@ -14,19 +41,21 @@
       </li>
     </ul>
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <router-link class="nav-link" to="/login">Login</router-link>
+      <li class="nav-item" id="username">
+        <router-link class="nav-link text-warning" to="/user">{{txt_user}}</router-link>
       </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/logout">Logout</router-link>
+      <li class="nav-item" id="btn_login">
+        <router-link class="nav-link" to="/" @click="disableLoginBtn">{{txt_login}}</router-link>
+      </li>
+      <li class="nav-item" id="btn_logout">
+        <router-link class="nav-link" to="/" @click="disableLogoutBtn">{{txt_logout}}</router-link>
       </li>
     </ul>
   </div>
 </nav>
 </template>
 
-<script>
-</script>
+
 
 <style>
 </style>
