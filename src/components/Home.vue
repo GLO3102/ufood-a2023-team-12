025f71276a2e0b98d2afd6be403fb60cbce1d940
EmnaBenRached <b.rached.emna@gmail.com>
2023-10-08 21:40:37 -0400

    added function generateStarRating and modified style of page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@ import {getAllGenres} from "../api/filters.js";
 
 
 const restaurants = ref([]);
-// console.log("rest1 ", restaurants);
+console.log("rest1 ", restaurants);
 onMounted(async () => {
   try {
     const fetchedRestaurants = await getRestaurants();
@@ -67,6 +67,19 @@ const clearFilters = () => {
     selectedRatings.value[rating] = false;
   }
 };
+
+function generateStarRating(rating) {
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 !== 0;
+
+  const stars = Array(fullStars).fill('<font-awesome-icon icon="fa-solid fa-star" />');
+
+  if (hasHalfStar) {
+    stars.push('<font-awesome-icon icon="fa-solid fa-star-half-stroke" />');
+  }
+  console.log('Stars generated:', stars);
+  return stars.join('');
+}
 </script>
 
 <template>
@@ -185,303 +198,17 @@ const clearFilters = () => {
 
                 <div class="restaurant_info">
                   <h2>{{ restaurant.name }}</h2>
-                  <p>Address: {{ restaurant.address }}</p>
-                  <h3>Price Range: {{ restaurant.price_range }}</h3>
-                  <h3>Genre: {{ restaurant.genres }}</h3>
-                  <div class="restaurant_star">
-                  </div>
+                  <p><span class="highlight">Address:</span> {{ restaurant.address }}</p>
+                  <h3><span class="highlight">Price Range:</span> {{ restaurant.price_range }}</h3>
+                  <h3><span class="highlight">Genre:</span> {{ restaurant.genres }}</h3>
+                  <div class="restaurant_star" v-html="generateStarRating(restaurant.rating)"></div>
+                    <!-- {{ generateStarRating(restaurant.rating) }} -->
+                  
                 </div>
               </div>
             </div>
         </div>
       </section>
-      <!-- <section class="restaurant" id="restaurant">
-        <div class="restaurant_box">
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <router-link class="nav-link" to="/restaurant">
-                <img
-                  src="https://images.unsplash.com/photo-1586816001966-79b736744398?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
-                  alt="Restaurant A Image"
-                />
-              </router-link>
-            </div>
-
-            <div class="restaurant_info">
-              <h2>Gourmet Corner</h2>
-              <p>Address: 123 Republic Street</p>
-              <h3>Price Range: Cheap</h3>
-              <h3>Genre: Fast Food</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-            <div class="restaurant_info">
-              <h2>Savory Delights</h2>
-              <p>Address: 456 Elm Avenue</p>
-              <h3>Price Range: Expensive</h3>
-              <h3>Genre: Healthy Food</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-
-            <div class="restaurant_info">
-              <h2>Taste Sensations</h2>
-              <p>Address: 999 Savor Street</p>
-              <h3>Price Range: Moderate</h3>
-              <h3>Genre: Desert</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-            <div class="restaurant_info">
-              <h2>Street Cafe</h2>
-              <p>Address: 101 Gourmet Road</p>
-              <h3>Price Range: Moderate</h3>
-              <h3>Genre: Breakfast</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1653542773816-3922a928b28c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-            <div class="restaurant_info">
-              <h2>The Gourmet</h2>
-              <p>Address: 555 Delight Drive</p>
-              <h3>Price Range: Expensive</h3>
-              <h3>Genre: Asian</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1565299543923-37dd37887442?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1981&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-            <div class="restaurant_info">
-              <h2>Palace of Tastes</h2>
-              <p>Address: 369 Foodie Lane</p>
-              <h3>Price Range: Cheap</h3>
-              <h3>Genre: Desert</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-            <div class="restaurant_info">
-              <h2>Yummy Pizza</h2>
-              <p>Address: 777 Star Street</p>
-              <h3>Price Range: Cheap</h3>
-              <h3>Genre: Fast Food</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1610441009633-b6ca9c6d4be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-
-            <div class="restaurant_info">
-              <h2>Savory Delights</h2>
-              <p>Address: 55 Republic Street</p>
-              <h3>Price Range: Moderate</h3>
-              <h3>Genre: Healthy Food</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1564327368633-151ef1d45021?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-
-            <div class="restaurant_info">
-              <h2>Cafe Serendipity</h2>
-              <p>Address: 123 Republic Street</p>
-              <h3>Price Range: Cheap</h3>
-              <h3>Genre: Breakfast</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1617196034596-33d3311524c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1980&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-            <div class="restaurant_info">
-              <h2>Suchi and Movie</h2>
-              <p>Address: 587 Star Street</p>
-              <h3>Price Range: Moderate</h3>
-              <h3>Genre: Asian</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://plus.unsplash.com/premium_photo-1675806257134-2d3e30833d05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-
-            <div class="restaurant_info">
-              <h2>Parisian Choice</h2>
-              <p>Address: 789 Oak Lane</p>
-              <h3>Price Range: Moderate</h3>
-              <h3>Genre: Desert</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-              </div>
-            </div>
-          </div>
-
-          <div class="restaurant_card">
-            <div class="restaurant_image">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1625940951329-4e8d09f87692?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80"
-                  alt="Restaurant A Image"
-                />
-              </a>
-            </div>
-
-            <div class="restaurant_info">
-              <h2>G_star</h2>
-              <p>Address: 78 Gourmet Road</p>
-              <h3>Price Range: Moderate</h3>
-              <h3>Genre: Fast Food</h3>
-              <div class="restaurant_star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star" />
-                <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> -->
     </div>
   </div>
 </template>
